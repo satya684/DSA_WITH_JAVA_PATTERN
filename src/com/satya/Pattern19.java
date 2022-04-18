76067496d12a26799c149f2121a42669bd2b807f
Satya <satya123cse@gmail.com>
2022-04-18 15:28:41 +0530

    19 and 20

---
+++ src/com/satya/Pattern19.java
@@ -0,0 +1,31 @@
+package com.satya;
+
+import java.util.Scanner;
+
+public class Pattern19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int st = n;
+        int sp = 0;
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= sp; j++) {
+                System.out.print("\t");
+            }
+            for (int j = 1; j <= st; j++) {
+//                if (i )
+                System.out.print("*\t");
+            }
+            if (i <= n / 2) {
+                sp++;
+                st -= 2;
+            } else {
+                sp--;
+                st += 2;
+            }
+            System.out.println();
+        }
+
+    }
+}
